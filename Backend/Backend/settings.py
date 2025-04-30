a6788ce076b96db9e5beb967e340720e1b12616b
AlvaroGaertnerGit <alvarogaertnerufv18@gmail.com>
2025-04-30 16:44:31 +0200

    Update settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'django-insecure-*j_cmtr=-1--2hvm-9hq83vo#fa87++=5@r^2bm3^fbqit$k)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['mathview-api.onrender.com']
 print(f"ALLOWED_HOSTS EN PRODUCCIÓN: {ALLOWED_HOSTS}")
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-
-
 
 # Application definition
 
